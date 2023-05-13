d3e8d625a7e4319db32cc6f6f2407daa2f07c0ae
KseniyaKsyusha <oksanai@online.ua>
2023-05-13 14:21:28 +0300

    hw 6

---
+++ hw6/script.js
@@ -0,0 +1,75 @@
+const users = [
+  {
+    index: 0,
+    isActive: true,
+    balance: "$2,226.60",
+    name: "Eugenia Sawyer",
+    gender: "female",
+    phone: "+1 (840) 583-3207",
+    address: "949 John Street, Rose, Puerto Rico, 1857",
+  },
+  {
+    index: 1,
+    isActive: true,
+    balance: "$2,613.77",
+    name: "Pauline Gallegos",
+    gender: "female",
+    phone: "+1 (985) 593-3328",
+    address: "328 Greenpoint Avenue, Torboy, North Dakota, 6857",
+  },
+  {
+    index: 2,
+    isActive: false,
+    balance: "$3,976.41",
+    name: "Middleton Chaney",
+    gender: "male",
+    phone: "+1 (995) 591-2478",
+    address: "807 Fleet Walk, Brutus, Arkansas, 9783",
+  },
+  {
+    index: 3,
+    isActive: true,
+    balance: "$1,934.58",
+    name: "Burns Poole",
+    gender: "male",
+    phone: "+1 (885) 559-3422",
+    address: "730 Seba Avenue, Osage, Alabama, 6290",
+  },
+  {
+    index: 4,
+    isActive: true,
+    balance: "$3,261.65",
+    name: "Mcfadden Horne",
+    gender: "male",
+    phone: "+1 (942) 565-3988",
+    address: "120 Scholes Street, Kirk, Michigan, 1018",
+  },
+  {
+    index: 5,
+    isActive: false,
+    balance: "$1,790.56",
+    name: "Suzette Lewis",
+    gender: "female",
+    phone: "+1 (837) 586-3283",
+    address: "314 Dunne Place, Bawcomville, Guam, 9053",
+  },
+];
+const validBalance =  /[^\d.]+/g;
+const minBalance = 2000;
+
+const filterBalance = users.filter(user => +user.balance.replace(validBalance, '') > minBalance);
+console.log(filterBalance);
+
+const phoneNumbers = filterBalance.map(user => user.phone);
+console.log(phoneNumbers);
+
+const allBalance = users.map(user => user.balance);
+console.log(allBalance)
+
+const arrayBalance = allBalance.map(user => +user.replace(validBalance, ''));
+console.log(arrayBalance)
+
+const overallBalance = 0;
+
+const totalBalance = arrayBalance.reduce((acc, value) => acc + value, overallBalance);
+console.log(totalBalance.toFixed(2))
