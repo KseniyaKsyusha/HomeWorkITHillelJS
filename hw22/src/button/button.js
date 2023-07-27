a58c0a2909f3186eea5e42c4b553958973f5947c
KseniyaKsyusha <109752337+KseniyaKsyusha@users.noreply.github.com>
2023-07-27 14:46:30 +0300

    Add files via upload

--- hw22/src/button/button.js
+++ hw22/src/button/button.js
@@ -1,1 +1,8 @@
+import $ from 'jquery';
+export const showButton = () => {
+    const button = document.createElement('button');
+    $('#container').append(button);
+    $(button).text('Click my');
+    $(button).attr('id', 'myButton');
 
+}
