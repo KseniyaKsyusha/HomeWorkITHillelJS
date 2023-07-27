835d5942a49e4da442be7e34a4af660c84af2148
KseniyaKsyusha <109752337+KseniyaKsyusha@users.noreply.github.com>
2023-07-27 14:45:29 +0300

    Add files via upload

--- hw22/src/main.js
+++ hw22/src/main.js
@@ -1,1 +1,31 @@
+import $ from "jquery";
+import './main.css';
+import image1 from "./image/image1.jpg";
+import image2 from "./image/image2.jpg"
+import { showButton } from './button/button';
 
+/* console.log($("body"));
+
+const picture = document.createElement('img');
+picture.setAttribute('src', image1);
+document.querySelector('body').appendChild(picture);
+
+const text = document.createElement('span');
+text.textContent = 'Contact!!!!'
+document.querySelector('body').appendChild(text);
+
+throw new Error ('Error');
+ */
+
+const divContainer = $('<div>').attr({'id':'container'});
+const picture = document.createElement('img');
+$(picture).attr('src', image1);
+$('body').append(divContainer);
+$('#container').append(picture);
+
+showButton();
+
+$('#myButton').click(function () {
+    $(picture).attr('src', image2);
+    $(this).hide();
+})
