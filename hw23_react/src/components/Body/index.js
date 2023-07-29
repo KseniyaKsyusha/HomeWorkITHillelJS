16cf91ee2fbfa0d9337ec3fa65238e576d4a6ce0
KseniyaKsyusha <109752337+KseniyaKsyusha@users.noreply.github.com>
2023-07-29 14:27:02 +0300

    Add files via upload

--- hw23_react/src/components/Body/index.js
+++ hw23_react/src/components/Body/index.js
@@ -1,1 +1,19 @@
+import { useState } from "react";
+import './style.css'
+import Left from "../Left"
+import Central from "../Central";
 
+const Body = () => {
+    const [src, setSrc] = useState('');
+   
+    return (
+    <div className='container'>
+        <Left callback={setSrc} />
+        <Central src={src}/>
+    </div>
+    )
+
+}
+
+
+export default Body;
