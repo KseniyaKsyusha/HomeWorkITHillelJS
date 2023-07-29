3d52f7d97b1f327d46d0e5f8a14f77290f2064b9
KseniyaKsyusha <109752337+KseniyaKsyusha@users.noreply.github.com>
2023-07-29 14:42:51 +0300

    Add files via upload

--- hw23_react/src/components/Header/index.js
+++ hw23_react/src/components/Header/index.js
@@ -1,1 +1,11 @@
+import './style.css'
 
+const Header = () => {
+    return (
+        <div className='headerContainer'>
+            <h1>Dogs!</h1>
+        </div>
+    );
+}
+
+export default Header;
