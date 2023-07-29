20b6e2020c7f3ccc7b80f07a3aed3b29f626687d
KseniyaKsyusha <109752337+KseniyaKsyusha@users.noreply.github.com>
2023-07-29 14:44:19 +0300

    Add files via upload

--- hw23_react/src/containers/App/App.js
+++ hw23_react/src/containers/App/App.js
@@ -1,1 +1,19 @@
+import './App.css';
 
+import Header from '../../components/Header';
+import Body from '../../components/Body';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+        <Header/>
+        <Body/>
+               
+      </header>
+    </div>
+  );
+}
+
+export default App;
