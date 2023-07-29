aa8494deb2c58e72d41b438f51cdbdbf348e27be
KseniyaKsyusha <109752337+KseniyaKsyusha@users.noreply.github.com>
2023-07-29 14:43:37 +0300

    Add files via upload

--- hw23_react/src/components/Left/index.js
+++ hw23_react/src/components/Left/index.js
@@ -1,1 +1,24 @@
+import { useState } from "react";
 
+import './style.css';
+import Button from './../Button';
+import image1 from './../../assets/image1.jpg';
+import image2 from './../../assets/image2.jpg';
+
+
+
+const Left = (props) => {
+    const [data, setData] = useState('');
+
+    props.callback(data);
+    
+    return (
+        <div className="left-container">
+            <Button name = 'Dog 1' data={image1} onClick={setData} />
+            <Button name = 'Dog 2' data ={image2} onClick={setData} />
+            
+        </div>
+    );
+}
+
+export default Left;
