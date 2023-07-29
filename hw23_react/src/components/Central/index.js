9f6efb5fa35a0e240f8f2b9152e9e2ed071d50b7
KseniyaKsyusha <109752337+KseniyaKsyusha@users.noreply.github.com>
2023-07-29 14:42:14 +0300

    Add files via upload

--- hw23_react/src/components/Central/index.js
+++ hw23_react/src/components/Central/index.js
@@ -1,1 +1,11 @@
+import './style.css'
 
+const  Central=(props) => {
+    return (
+        <div className="central-container">
+            <img src={props.src} className='myImage'></img>
+        </div>
+    )
+}
+
+export default Central;
